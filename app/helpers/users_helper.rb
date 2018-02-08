4c88a78955bb5de514ac8fc39efc10e7c931c4f9
ShubhamKhandare <shubhamkhandare12@gmail.com>
2018-02-08 13:17:02 +0530

    initials as avatar current user name in partial

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,11 +4,18 @@ module UsersHelper
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
 		# <%= image_tag(current_user.avatar.url(:thumb)) %>
 		# image_tag(gravatar_url, alt: user.name, class: "gravatar")
-		
-		image_tag(user.avatar.url(:thumb), alt: user.name, class: "gravatar")
+		if user.avatar.url == "https://secure.gravatar.com/avatar/s=60/d=identicon"
+			fullname = user.name.split(" ")
+			initials_url = "https://ui-avatars.com/api/?name=#{fullname.first}+#{fullname.last}&background=0D8ABC&color=fff&rounded=true"
+			image_tag(initials_url, alt: user.name, class: "gravatar")
+		else
+			image_tag(user.avatar.url(:thumb), alt: user.name, class: "gravatar")
+		end
 	end
 	
 	def current_user?(user)
 		user ==  current_user
 	end
 end
+
+
